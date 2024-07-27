51ce77a5b667683d29c7158d45d4825aa3de71d3
haris hamdani <harishamdani@users.noreply.github.com>
2024-07-27 09:31:09 +0200

    add test

--- TournamentSimulator.UnitTests/Services/MatchSimulatorTests.cs
+++ TournamentSimulator.UnitTests/Services/MatchSimulatorTests.cs
@@ -42,5 +42,29 @@ namespace TournamentSimulator.UnitTests.Services
             simulatedMatches.Should().HaveCount(3);
             simulatedMatches.Should().OnlyContain(m => m.HomeGoals >= 0 && m.AwayGoals >= 0);
         }
+        
+        [Test]
+        public void SimulateMatch_ShouldGiveHomeTeamAdvantage()
+        {
+            var matchSimulator = new MatchSimulator();
+            var homeTeam = new Team("Home", 50);
+            var awayTeam = new Team("Away", 50);
+    
+            int totalHomeGoals = 0;
+            int totalAwayGoals = 0;
+            int simulations = 1000;
+
+            for (int i = 0; i < simulations; i++)
+            {
+                var match = matchSimulator.SimulateMatch(homeTeam, awayTeam);
+                totalHomeGoals += match.HomeGoals;
+                totalAwayGoals += match.AwayGoals;
+            }
+
+            double averageHomeGoals = (double)totalHomeGoals / simulations;
+            double averageAwayGoals = (double)totalAwayGoals / simulations;
+
+            averageHomeGoals.Should().BeGreaterThan(averageAwayGoals);
+        }
     }
 }
